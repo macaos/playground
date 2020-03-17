02c602c4b413fbb6bb2c81a1c563fe14bfbc480a
macaos <macaoshu@gmail.com>
2020-03-18 02:04:34 +0900

    test build

---
+++ build/precache-manifest.7c5c1ef4375c87e8ea992a5dc02cefa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39eb53d0b4c2cca1f2a179a87a32a297",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a1a654ecb37b8a6dfc0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52a63b6dcfa6475543cd",
+    "url": "/static/js/2.d3c5dbb8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d3c5dbb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1a654ecb37b8a6dfc0",
+    "url": "/static/js/main.7e25e1bf.chunk.js"
+  },
+  {
+    "revision": "5586f65fa0adc24410bd",
+    "url": "/static/js/runtime-main.cf6da0ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
